eeaf9d093b764a480825d4896efd53e5d9810cd7
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2017-01-08 14:20:43 +0000

    objects found and working in console

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 class Projects extends Component {
   render() {
+    let projectItems;
+    if(this.props.projects){
+      projectItems = this.props.projects.map(project => {
+        console.log(project);
+      })
+    }
+    console.log(this.props);
     return (
       <div className="Projects">
       My Projects
